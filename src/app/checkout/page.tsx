4373ce032420d58b861af2272664fc6373df6bbb
Leandro Martínez <leandro.martinez17@outlook.com>
2025-07-07 23:09:19 -0300

    fix: metadata

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
     title: "Checkout",
     description: "Complete your purchase securely and efficiently.",
+    keywords: "checkout, ecommerce, payment, secure purchase, shopping cart"
 };
 
 const CheckoutPage = () => {
