ae2ee515e9f2c20c37c95371ebe2f614435c9fba
Leandro Martinez <leandromartinez.dev@gmail.com>
2025-07-11 17:57:31 -0300

    fix: styles and go products button

--- src/components/favorites/FavoritesList.tsx
+++ src/components/favorites/FavoritesList.tsx
@@ -3,11 +3,12 @@
 import { useAppStore } from '@/store'
 import FavoritesItem from './FavoritesItem'
 import PageTitle from '../titles/PageTitle'
+import LinkToHomeButton from '../ui/LinkToHomeButton'
 
 const FavoritesList = () => {
     const favorites = useAppStore((state) => state.favorites)
     return (
-        <div className="mih-h-screen h-auto bg-gray-100 py-8">
+        <div className="min-h-screen h-auto bg-gray-100 py-8">
             <PageTitle>
                 Favorites
             </PageTitle>
@@ -16,6 +17,17 @@ const FavoritesList = () => {
                     {favorites.map((prd) => (
                         <FavoritesItem key={prd.id} product={prd} />
                     ))}
+                    {favorites.length === 0 && (
+                        <div className='flex flex-col items-center justify-center gap-y-10'>
+                            <div className="text-center text-gray-500">
+                                No favorites added yet.
+                            </div>
+                            <LinkToHomeButton
+                                href='/'
+                                text='Go back to shopping'
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
